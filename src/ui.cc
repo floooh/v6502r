4574e829fa1d7b9bb2e55dba6afd59d0f677e571
Andre Weissflog <floooh@gmail.com>
2023-01-13 17:41:30 +0100

    change the Contrast theme background to white

--- src/ui.cc
+++ src/ui.cc
@@ -771,7 +771,7 @@ static void ui_menu(void) {
                         { 1.0f, 0.3f, 0.3f, 1.0f },
                         { 0.5f, 0.1f, 0.75f, 1.0f },
                     },
-                    { 0.8f, 0.8f, 0.8f, 1.0f }
+                    { 1.0f, 1.0f, 1.0f, 1.0f }
                 });
             }
             if (ImGui::MenuItem("Matrix")) {
