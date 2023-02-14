904f0d2df142e2c437f00d8d6fea4011df9c4bbc
Andre Weissflog <floooh@gmail.com>
2023-02-14 19:05:10 +0100

    remove SOKOL_LOG define, no longer needed

--- ext/sokol/sokol_defines.h
+++ ext/sokol/sokol_defines.h
@@ -1,7 +1,6 @@
 #define SOKOL_TRACE_HOOKS
 #if defined(_MSC_VER)
     #define SOKOL_D3D11
-    #define SOKOL_LOG(str) OutputDebugStringA(str)
 #elif defined(__EMSCRIPTEN__)
     #define SOKOL_GLES2
 #elif defined(__APPLE__)
