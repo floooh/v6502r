13c4a11bf604a4699733b5ff0bafb6bff702cb07
Andre Weissflog <floooh@gmail.com>
2025-12-27 19:46:15 +0100

    port build system to fibs wip

--- fibs.ts
+++ fibs.ts
@@ -25,38 +25,124 @@ export function configure(c: Configurer) {
 }
 
 export function build(b: Builder) {
+    b.addIncludeDirectories(['ext']);
+    const commonSources = [
+        "main.c",
+        "input.c",
+        "input.h",
+        "util.c",
+        "util.h",
+        "gfx.c",
+        "gfx.h",
+        "pick.c",
+        "pick.h",
+        "sim.c",
+        "sim.h",
+        "trace.c",
+        "trace.h",
+        "asm.c",
+        "asm.h",
+        "ui.cc",
+        "ui.h",
+        "ui_asm.cc",
+        "ui_asm.h",
+        "imgui_util.cc",
+        "imgui_util.h",
+        "common.h",
+    ];
+    b.addTarget("v6502r", "windowed-exe", (t) => {
+        t.setDir("src");
+        t.addSources(commonSources);
+        t.addIncludeDirectories(['m6502']);
+        t.addSources(["m6502/segdefs.c", "m6502/segdefs.h"]);
+        t.addSources(["m6502/nodenames.c", "m6502/nodenames.h", "m6502/nodegroups.h"]);
+        t.addDependencies(['assets', 'perfect6502', 'asmx_6502' ]);
+        t.addCompileDefinitions({ CHIP_6502: "1" });
+        if (b.isEmscripten()) {
+            t.addLinkOptions([`--shell-file ${b.projectDir()}/src/m6502/shell.html`]);
+        }
+    });
+    b.addTarget("vz80r", "windowed-exe", (t) => {
+        t.setDir("src");
+        t.addSources(commonSources);
+        t.addIncludeDirectories(['z80']);
+        t.addSources(["z80/segdefs.c", "z80/segdefs.h"]);
+        t.addSources(["z80/nodenames.c", "z80/nodenames.h", "z80/nodegroups.h"]);
+        t.addDependencies(['assets', 'perfectz80', 'asmx_z80' ]);
+        t.addCompileDefinitions({ CHIP_Z80: "1" });
+        if (b.isEmscripten()) {
+            t.addLinkOptions([`--shell-file ${b.projectDir()}/src/z80/shell.html`]);
+        }
+    });
+    b.addTarget("v2a03r", "windowed-exe", (t) => {
+        t.setDir("src");
+        t.addSources(commonSources);
+        t.addIncludeDirectories(['2a03']);
+        t.addSources(["2a03/segdefs.c", "2a03/segdefs.h"]);
+        t.addSources(["2a03/nodenames.c", "2a03/nodenames.h", "2a03/nodegroups.h"]);
+        t.addDependencies(['assets', 'perfect2a03', 'asmx_6502' ]);
+        t.addCompileDefinitions({ CHIP_2A03: "1" });
+        if (b.isEmscripten()) {
+            t.addLinkOptions([`--shell-file ${b.projectDir()}/src/2a03/shell.html`]);
+        }
+    });
+
     b.addTarget("asmx_6502", "lib", (t) => {
         t.setDir("ext/asmx/src");
-        t.addSources(['asmx.c', 'asmx.h', 'asm6502.c']);
-        t.addIncludeDirectories(['.']);
-        t.addCompileDefinitions({ ASMX_6502: '1' });
+        t.addSources(["asmx.c", "asmx.h", "asm6502.c"]);
+        t.addIncludeDirectories(["."]);
+        t.addCompileDefinitions({ ASMX_6502: "1" });
         if (b.isGcc() || b.isClang()) {
-            t.addCompileOptions(['-Wno-implicit-fallthrough']);
-        };
+            t.addCompileOptions(["-Wno-implicit-fallthrough"]);
+        }
     });
     b.addTarget("asmx_z80", "lib", (t) => {
         t.setDir("ext/asmx/src");
-        t.addSources(['asmx.c', 'asmx.h', 'asmz80.c']);
-        t.addIncludeDirectories(['.']);
-        t.addCompileDefinitions({ ASMX_Z80: '1' });
+        t.addSources(["asmx.c", "asmx.h", "asmz80.c"]);
+        t.addIncludeDirectories(["."]);
+        t.addCompileDefinitions({ ASMX_Z80: "1" });
         if (b.isGcc() || b.isClang()) {
-            t.addCompileOptions({ opts: ['-Wno-implicit-fallthrough'], scope: 'private' });
-        };
+            t.addCompileOptions({
+                opts: ["-Wno-implicit-fallthrough"],
+                scope: "private",
+            });
+        }
     });
     b.addTarget("perfect6502", "lib", (t) => {
         t.setDir("ext/perfect6502");
-        t.addSources(["netlist_6502.h", "types.h", "netlist_sim.h", "netlist_sim.c", "perfect6502.h", "perfect6502.c"]);
-        t.addIncludeDirectories(['.']);
+        t.addSources([
+            "netlist_6502.h",
+            "types.h",
+            "netlist_sim.h",
+            "netlist_sim.c",
+            "perfect6502.h",
+            "perfect6502.c",
+        ]);
+        t.addIncludeDirectories(["."]);
     });
     b.addTarget("perfectz80", "lib", (t) => {
         t.setDir("ext/perfect6502");
-        t.addSources(["netlist_z80.h", "types.h", "netlist_sim.h", "netlist_sim.c", "perfectz80.h", "perfectz80.c"]);
-        t.addIncludeDirectories(['.']);
+        t.addSources([
+            "netlist_z80.h",
+            "types.h",
+            "netlist_sim.h",
+            "netlist_sim.c",
+            "perfectz80.h",
+            "perfectz80.c",
+        ]);
+        t.addIncludeDirectories(["."]);
     });
     b.addTarget("perfect2a03", "lib", (t) => {
         t.setDir("ext/perfect6502");
-        t.addSources(["netlist_2a03.h", "types.h", "netlist_sim.h", "netlist_sim.c", "perfect2a03.h", "perfect2a03.c"]);
-        t.addIncludeDirectories(['.']);
+        t.addSources([
+            "netlist_2a03.h",
+            "types.h",
+            "netlist_sim.h",
+            "netlist_sim.c",
+            "perfect2a03.h",
+            "perfect2a03.c",
+        ]);
+        t.addIncludeDirectories(["."]);
     });
     b.addTarget("sokolimpl", "lib", (t) => {
         t.setDir("ext/sokol");
@@ -66,27 +152,37 @@ export function build(b: Builder) {
     b.addTarget("texteditor", "lib", (t) => {
         t.setDir("ext/texteditor");
         t.addSources(["TextEditor.cpp", "TextEditor.h"]);
-        t.addIncludeDirectories(['.']);
+        t.addIncludeDirectories(["."]);
         t.addDependencies(["imgui-docking"]);
         if (b.isClang()) {
-            t.addCompileOptions({ opts: ['-Wno-unused-but-set-variable'], scope: 'private' });
+            t.addCompileOptions({
+                opts: ["-Wno-unused-but-set-variable"],
+                scope: "private",
+            });
         }
     });
     b.addTarget("assets", "interface", (t) => {
-        t.setDir("src/res");
-        t.addSources(['help_assembler.md', 'about.md'])
+        const shdcOutDir = t.buildDir();
+        t.setDir("src");
+        t.addSources(["gfx.glsl", "res/help_assembler.md", "res/about.md"]);
+        t.addJob({
+            job: "sokolshdc",
+            args: { src: "gfx.glsl", outDir: shdcOutDir },
+        });
+        t.addIncludeDirectories({ dirs: [shdcOutDir], scope: "interface" });
         t.addJob({
             job: "embedfiles",
-            args: { outHeader: "fonts.h", files: ["fontawesome.ttf"] },
+            args: { outHeader: "res/fonts.h", prefix: "dump_", files: ["res/fontawesome.ttf"] },
         });
         t.addJob({
             job: "embedfiles",
             args: {
-                outHeader: "markdown.h",
+                outHeader: "res/markdown.h",
                 asText: true,
-                files: ["help_assembler.md", "about.md"],
+                prefix: "dump_",
+                files: ["res/help_assembler.md", "res/about.md"],
             },
         });
-        t.addDependencies(['sokol', 'texteditor']);
+        t.addDependencies(["sokolimpl", "texteditor"]);
     });
 }
